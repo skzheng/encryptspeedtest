90f1c7ee0422f85e91a3407237f39d04b4f0cf08
Simon K Zheng <simonkzheng@gmail.com>
2018-08-06 10:12:14 -0400

    Test

--- server.js
+++ server.js
@@ -2,25 +2,62 @@ const express = require('express')
 const fs = require('fs')
 const path = require('path')
 const app = express()
+const crypto = require('crypto');
+const KEY = 'TEST'
+const cipher = crypto.createCipher('aes-256-ctr', KEY);
+const decipher = crypto.createDecipher('aes-256-ctr', KEY);
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.htm'))
+  res.sendFile(path.join(__dirname + '/public/index.html'))
 })
 
-app.get('/video', function(req, res) {
-  const path = 'assets/sample.mp4'
-  const stat = fs.statSync(path)
-  const fileSize = stat.size
-  const range = req.headers.range
+app.get('/decrypted', function(req, res) {
+  const path = 'assets/sample.mp4';
+  const stat =fs.statSync(path);
+  const fileSize = stat.size;
+  const range = req.headers.range; 
+
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-");
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] 
+      ? parseInt(parts[1], 10)
+      : fileSize - 1;
+    
+    const chunksize = (end - start) + 1;
+    const file = fs.createReadStream(path, {start, end});
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4'
+    }
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4'
+    }
+    res.writeHead(200, head);
+    fs.createReadStream(path).pipe(res);
+  }
+});
+
+app.get('/encrypted', function(req, res) {
+  const path = 'assets/sample.mp4';
+  const stat = fs.statSync(path);
+  const fileSize = stat.size;
+  const range = req.headers.range;
 
   if (range) {
     const parts = range.replace(/bytes=/, "").split("-")
-    const start = parseInt(parts[0], 10)
+    const start = parseInt(parts[0], 10);
     const end = parts[1]
       ? parseInt(parts[1], 10)
-      : fileSize-1
+      : fileSize - 1;
 
     const chunksize = (end-start)+1
     const file = fs.createReadStream(path, {start, end})
@@ -31,15 +68,17 @@ app.get('/video', function(req, res) {
       'Content-Type': 'video/mp4',
     }
 
-    res.writeHead(206, head)
-    file.pipe(res)
+    res.writeHead(206, head);
+    file.pipe(res).pipe(cipher).pipe(decipher);
+    // file.pipe(res);
   } else {
     const head = {
       'Content-Length': fileSize,
       'Content-Type': 'video/mp4',
     }
-    res.writeHead(200, head)
-    fs.createReadStream(path).pipe(res)
+    res.writeHead(200, head);
+    fs.createReadStream(path).pipe(res).pipe(cipher).pipe(decipher);
+    // fs.createReadStream(path).pipe(res);
   }
 })
 
