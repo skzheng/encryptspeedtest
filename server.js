e06e092605e3d2cd6d15e483e033cd4db11ccfb4
Simon K Zheng <simonkzheng@gmail.com>
2018-08-07 12:34:21 -0400

    Update

--- server.js
+++ server.js
@@ -88,67 +88,21 @@ app.get('/encrypted', function(req, res) {
   }
 });
 
-// app.get('/s3encrypted', function(req, res) {
-//   // let file = fs.createWriteStream("efile.mp4");
-
-//   let input = fs.createReadStream
-//   let bufferStream = new stream.PassThrough();
-
-//   let request = http.get("http://s3.amazonaws.com/prototype.sesamecms.org/large-video/test.mp4 ", function(response) {
-//     response.pipe(res)
-//     // let decrypted = decrypt(response);
-//     // decrypted.pipe(res);
-//     // let vid = response.pipe(file);
-//     // vid.on('finish', function() {
-//     //   fs.createReadStream("efile.mp4").pipe(res).pipe(s3decipher);
-//     // });
-//     input.pipe(s3decipher).pipe(bufferStream, { end: true });
-    
-//     bufferStream.on('readable', () => {
-//       let chunk = bufferStream.read(16 * 1024);
-//       if (chunk) {
-//         if ((start - length >= 0) && (start - length < 16 * 1024)) {
-//           dataReady = true;
-//         }
-//         length += chunk.length;
-//         if (dataReady) {
-//           res.statusCode = 206;
-//           res.write(chunk);
-//         }
-//       }
-//     });
-//     bufferStream.on('end', () => {
-//       res.end();
-//     });
-//   });
-//   // function decrypt(buff) {
-//   //   return Buffer.concat([
-//   //     s3decipher.update(buff),
-//   //     s3decipher.final()
-//   //   ]).toString();
-//   // }
-// });
-
 app.get('/s3encrypted', function(req, res) {
-  // let file = fs.createWriteStream("file.mp4");
-  // let request = http.get("http://s3.amazonaws.com/prototype.sesamecms.org/large-video/test-e.mp4", function(response) {
-    
-  //   // let decoded = decipher.update(response, 'binary', 'utf8');
-  //   // decoded += decipher.final('utf8');
-
-  //   // decoded.pipe(res);
-  //   // let vid = response.pipe(file);
-  //   // vid.on('finish', function () {
-  //   //   fs.createReadStream("file.mp4").pipe(res);
-  //   // });
+  // let request = http.get("http://s3.amazonaws.com/prototype.sesamecms.org/large-video/test-e.mp4", function(response) {  
   // });
   request.get("http://s3.amazonaws.com/prototype.sesamecms.org/large-video/test-e.mp4").pipe(s3decipher).pipe(res);
 });
 app.get('/s3decrypted', function(req, res) {
+  // # 1
+
   // let request = http.get("http://s3.amazonaws.com/prototype.sesamecms.org/large-video/test.mp4", function(response) {
-  // //   // console.log(response);
+    // console.log(response);
   //   response.pipe(res);
   // });
+
+  // # 2
+  
   request.get("http://s3.amazonaws.com/prototype.sesamecms.org/large-video/test.mp4").pipe(res);
 });
 
